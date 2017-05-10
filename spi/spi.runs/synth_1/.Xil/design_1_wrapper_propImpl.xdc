4980c3466db99809b00dba189c36a2e477689143
Atal <atal.bajracharya@gmailcom>
2017-05-10 11:26:38 -0400

    1st commit.

---
+++ spi/spi.runs/synth_1/.Xil/design_1_wrapper_propImpl.xdc
@@ -0,0 +1,9 @@
+set_property SRC_FILE_INFO {cfile:C:/College/Thesis/spi_test/spi/spi.srcs/constrs_1/new/spi.xdc rfile:../../../spi.srcs/constrs_1/new/spi.xdc id:1} [current_design]
+set_property src_info {type:XDC file:1 line:1 export:INPUT save:INPUT read:READ} [current_design]
+set_property -dict { PACKAGE_PIN B13 IOSTANDARD LVCMOS33 } [get_ports { MOSI }];
+set_property src_info {type:XDC file:1 line:2 export:INPUT save:INPUT read:READ} [current_design]
+set_property -dict { PACKAGE_PIN F14 IOSTANDARD LVCMOS33 } [get_ports { MISO }];
+set_property src_info {type:XDC file:1 line:3 export:INPUT save:INPUT read:READ} [current_design]
+set_property -dict { PACKAGE_PIN D17 IOSTANDARD LVCMOS33 } [get_ports { SCLK }];
+set_property src_info {type:XDC file:1 line:4 export:INPUT save:INPUT read:READ} [current_design]
+set_property -dict { PACKAGE_PIN E17 IOSTANDARD LVCMOS33 } [get_ports { CS }];
---
+++ spi/spi.srcs/sources_1/bd/design_1/hdl/design_1_wrapper.v
@@ -0,0 +1,48 @@
+//Copyright 1986-2016 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2016.3 (win64) Build 1682563 Mon Oct 10 19:07:27 MDT 2016
+//Date        : Wed May 10 10:29:42 2017
+//Host        : LAPTOP-L1N8U9P6 running 64-bit major release  (build 9200)
+//Command     : generate_target design_1_wrapper.bd
+//Design      : design_1_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module design_1_wrapper
+   (CS,
+    MISO,
+    MOSI,
+    SCLK,
+    reset,
+    sys_clock,
+    usb_uart_rxd,
+    usb_uart_txd);
+  output [0:0]CS;
+  input MISO;
+  output MOSI;
+  output SCLK;
+  input reset;
+  input sys_clock;
+  input usb_uart_rxd;
+  output usb_uart_txd;
+
+  wire [0:0]CS;
+  wire MISO;
+  wire MOSI;
+  wire SCLK;
+  wire reset;
+  wire sys_clock;
+  wire usb_uart_rxd;
+  wire usb_uart_txd;
+
+  design_1 design_1_i
+       (.CS(CS),
+        .MISO(MISO),
+        .MOSI(MOSI),
+        .SCLK(SCLK),
+        .reset(reset),
+        .sys_clock(sys_clock),
+        .usb_uart_rxd(usb_uart_rxd),
+        .usb_uart_txd(usb_uart_txd));
+endmodule
